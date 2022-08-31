3280b2a3075a27d02b43885f05dacf069d14688e
Yun Jang <jang7j@yahoo.com>
2022-08-31 11:01:35 -0500

    initial commit

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrome(string word)
+        {
+            
+            var array = word.ToLower().ToCharArray();
+            bool result = false;
+            for (var i = 0; i < array.Length; i++)
+            {
+                if (array[i] == array[^(i + 1)])
+                {
+                    result = true;
+                }
+                else
+                {
+                    return false;
+                }
+
+            }
+            return result;
+           
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,10 +6,20 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("racecar", true)]
+        [InlineData("wow", true)]
+        [InlineData("Hannah", true)]
+        [InlineData("crazy", false)]
+        [InlineData("great", false)]
+        public void Test1(string word, bool expected)
         {
-
+            
+            var wordsmith = new WordSmith();
+            var actual = wordsmith.IsAPalindrome(word);
+            Assert.Equal(expected, actual);
         }
+
+        
     }
 }
